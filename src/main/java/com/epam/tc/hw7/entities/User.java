62c44a6054c0b23e7d44dd2b8e34177c3edcf608
Alex-apple1 <72156173+Alex-apple1@users.noreply.github.com>
2022-08-28 09:47:28 +0300

    mu old homework

---
+++ src/main/java/com/epam/tc/hw7/entities/User.java
@@ -0,0 +1,15 @@
+package com.epam.tc.hw7.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum User {
+    ROMAN("Roman", "Jdi1234", "ROMAN IOVLEV");
+
+    private final String name;
+    private final String password;
+    private final String fullName;
+
+}
